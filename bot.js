8782bf795091d044ec4e603c804ffa1f73de2206
kpittman23 <kateyluvyou@gmail.com>
2016-10-09 03:01:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -96,7 +96,7 @@ function decode(saveCode){
 	checkpoint = digitArray[13];
 	
 	postMessage("Welcome back! Attack: " + attack.toString() + "| Defense: " + defense.toString() + "| Evasiveness: " + evasivness.toString(),
-			"| Accuracy: " accuracy.toString() + "| Health Points: " + hp.toString() + "| Speed: " + speed.toString());
+			"| Accuracy: " + accuracy.toString() + "| Health Points: " + hp.toString() + "| Speed: " + speed.toString());
 	
 }
 
