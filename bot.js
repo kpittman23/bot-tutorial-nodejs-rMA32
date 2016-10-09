f4b8eec7ee8e2811370e8c25b92b247a8eff1b7b
kpittman23 <kateyluvyou@gmail.com>
2016-10-08 21:24:10 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegexKya = /(.|)*(k|K)ya!~/;
       botname = /(.|)*kyaa!~/;
-      botsave = /^save^/;
+      botsave = /(.|)*\bsave\b/;
   
   var waifuPhrases = [ "https://pbs.twimg.com/media/B8YdqjxIQAAU87L.jpg", "It's not like I l-like you or anything...", 
                       "B-B-baka!", "My senpai is the best!", "But isn't that... lewd?", "Kemy-kun is sugoi, but not as sugoi as senpai!", "Noooo!",
@@ -39,7 +39,7 @@ function respond() {
 function saveProgress(){
   var saveCode = 111111111111111;
   var newFirstDig = saveCode % 10000000000000000;
-  postMessage("your save code is " + saveCode.toString() + " and your new first digit is " + newFirstDig.toString());
+  postMessage("your saved code is " + saveCode.toString() + " and your new first digit is " + newFirstDig.toString());
 }
 
 function postMessage(response) {
